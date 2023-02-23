3d39d9108dde02d29479806ff5a39b9185cdc0fd
Rojoin <nachoarrastua@gmail.com>
2023-02-23 01:04:12 -0300

    Add UIScore

---
+++ Assets/Script/UIScore.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class UIScore : MonoBehaviour
+{
+    private float score;
+    private TextMeshProUGUI textMesh;
+
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        textMesh.text = score.ToString("0");
+    }
+
+   public void SetScore(uint score)
+    {
+        this.score = score;
+    }
+}
