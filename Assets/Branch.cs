16dbd8e748b5e1c9fc4dd75a50d9de78c00351b1
Rojoin <nachoarrastua@gmail.com>
2023-02-23 03:18:56 -0300

    Add Branch & TreeGenerator

---
+++ Assets/Branch.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    private PlayerController.Position freePosition;
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SetFreePosition(int x)
+    {
+        freePosition = (PlayerController.Position)x;
+    }
+    PlayerController.Position GetFreePosition() => freePosition;
+}
