803cf0ed07e208aabb4c92d53d84253be062de73
Rojoin <nachoarrastua@gmail.com>
2023-02-24 12:43:20 -0300

    Add TreeGenerator

--- Assets/Script/TreeGenerator.cs
+++ Assets/Script/TreeGenerator.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TreeGenerator : MonoBehaviour
 {
-    
+    public Branch[] branch;
+    public Transform[] branchPlaces;
     void Start()
     {
-        
+        int x = 0;
+        foreach (var branches in branch)
+        {
+            branches.SetFreePosition(Random.Range(0,2));
+            branches.gameObject.transform.position = branchPlaces[x].position;
+            x++;
+        }
     }
 
 
     void Update()
     {
-        
+
     }
 }
