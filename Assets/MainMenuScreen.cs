14fcefad898a8fbd046e13e146b1461ccb7b9adf
Rojoin <nachoarrastua@gmail.com>
2023-03-02 03:45:03 -0300

    Add MainMenuScreen

---
+++ Assets/MainMenuScreen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScreen : MonoBehaviour
+{
+    [SerializeField] PlayerController player;
+    [SerializeField] TreeGenerator tree;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Play()
+    {
+        tree.Reset();
+        player.Reset();
+        player.SetFirstTimeState(true);
+        player.SetGameState(true);
+        this.gameObject.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
