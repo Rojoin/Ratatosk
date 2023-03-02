a67e00e81a936acfa4294d472a9a48e4115c4645
Rojoin <nachoarrastua@gmail.com>
2023-03-02 03:48:01 -0300

    Add MainMenu to Scene

--- Assets/UiController.cs
+++ Assets/UiController.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class UiController : MonoBehaviour
 {
     [SerializeField] GameObject screenGameOver;
-   // [SerializeField] GameObject screenPause;
+    [SerializeField] GameObject screenMenu;
+    // [SerializeField] GameObject screenPause;
     [SerializeField] PlayerController player;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameOverScreen();
+        if (player.IsGameplayOn())
+        {
+            GameOverScreen();
+        }
+        else
+        {
+            MainMenuScreen();
+        }
     }
-    void ScreenVisibility(GameObject screen,bool status)
+    void ScreenVisibility(GameObject screen, bool status)
     {
         screen.SetActive(status);
     }
     void GameOverScreen()
     {
-
         ScreenVisibility(screenGameOver, !player.isAlive());
-       
+    }
+    void MainMenuScreen()
+    {
+        ScreenVisibility(screenMenu, !player.IsGameplayOn());
     }
 
 }
