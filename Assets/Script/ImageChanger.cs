050667e181578ee5510722bfbc170d588f21e8cf
Rojoin <nachoarrastua@gmail.com>
2023-03-09 22:35:26 -0300

    Add Music & SFX Toggles

---
+++ Assets/Script/ImageChanger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageChanger : MonoBehaviour
+{
+    [SerializeField] private Sprite image, image2;
+    [SerializeField] private Image currentImage;
+    private void Start()
+    {
+            currentImage.sprite = image;
+    }
+    public void ChangeImage()
+    {
+        if (currentImage.sprite == image) currentImage.sprite = image2;
+        else currentImage.sprite = image;
+    }
+
+}
