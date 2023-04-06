d1243c232f973c31d5d54fe8460bddc72212692e
sergio-ic <sergiob@imagecampus.edu.ar>
2023-04-06 11:47:53 -0300

    Add back to menu in credits with Android back button

--- Assets/Script/CreditsMenuScreen.cs
+++ Assets/Script/CreditsMenuScreen.cs
@@ -2,17 +2,29 @@ using UnityEngine;
 
 public class CreditsMenuScreen : MonoBehaviour
 {
-    [SerializeField] GameObject MainMenuObject;
+    //[SerializeField] GameObject MainMenuObject;
+    [SerializeField] UiController uiController;
 
     void Start()
     {
         gameObject.SetActive(false);
     }
 
+#if UNITY_ANDROID
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Exit();
+        }
+    }
+#endif
+
     public void Exit()
     {
-        SoundManager.Instance.PlaySound(SoundManager.Instance.button);
-        MainMenuObject.SetActive(true);
-        this.gameObject.SetActive(false);
+        uiController.SetCredits();
+        //SoundManager.Instance.PlaySound(SoundManager.Instance.button);
+        //MainMenuObject.SetActive(true);
+        //this.gameObject.SetActive(false);
     }
 }
